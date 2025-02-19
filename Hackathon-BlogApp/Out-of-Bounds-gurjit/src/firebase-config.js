670c0cedbe88cf9279485015133400bf22e3d2fd
kirtan2003 <102241757+kirtan2003@users.noreply.github.com>
2025-02-19 21:05:29 +0530

    changed firebase config

--- Hackathon-BlogApp/Out-of-Bounds-gurjit/src/firebase-config.js
+++ Hackathon-BlogApp/Out-of-Bounds-gurjit/src/firebase-config.js
@@ -22,12 +22,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLkl3lYoys3HRUZIZZRR3FVuVGeiaVmAE",
-  authDomain: "blog-13b2c.firebaseapp.com",
-  projectId: "blog-13b2c",
-  storageBucket: "blog-13b2c.appspot.com",
-  messagingSenderId: "553484679768",
-  appId: "1:553484679768:web:d782097c6248d33c88282d"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
